0f48cee599932444d5a331da3c6be911412c7141
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2021-03-10 18:54:27 +0300

    feat: add one more condition for numbers below zero

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  return n.toString().split('').reverse().join('')
+    if (n >= 0) return n.toString().split('').reverse().join('')
+    else if (n < 0) return n.toString().split('').reverse().join('').slice(0,-1)
 }
